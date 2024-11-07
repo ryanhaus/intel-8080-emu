58ab50dccf4adb36e1df95969f1d7ca473bb6ca2
Ryan Hausmann <ryanhausmann3@gmail.com>
2024-11-07 16:18:39 -0500

    Modify Memory methods to return Result for better error handling

--- src/cpu/memory.rs
+++ src/cpu/memory.rs
@@ -16,25 +16,33 @@ impl Memory {
     }
 
     // reads a RegisterValue from the given address
-    pub fn read(&self, addr: RegisterValue, read_16: bool) -> RegisterValue {
+    pub fn read(&self, addr: RegisterValue, read_16: bool) -> Result<RegisterValue, String> {
         // convert addr to usize for array access
         let addr = u16::from(addr) as usize;
 
         // read the value
-        if read_16 {
+        let value = if read_16 {
+            // make sure this won't read outside of memory
+            if addr == 0xFFFF {
+                return Err(String::from("Attempt to read outside of memory"));
+            }
+
             // read little-endian 16-bit integer from memory
-            let lower = self.data[addr % 0x10000] as u16;
-            let higher = self.data[(addr + 1) % 0x10000] as u16;
+            let lower = self.data[addr] as u16;
+            let higher = self.data[addr + 1] as u16;
             let value = (higher << 8) + lower;
             RegisterValue::from(value)
         } else {
             // read single 8-bit integer from memory
             RegisterValue::from(self.data[addr])
-        }
+        };
+
+        // return the value
+        Ok(value)
     }
 
     // writes a RegisterValue to memory at the given address
-    pub fn write(&mut self, addr: RegisterValue, value: RegisterValue) {
+    pub fn write(&mut self, addr: RegisterValue, value: RegisterValue) -> Result<(), String> {
         // convert addr to usize for array access
         let addr = u16::from(addr) as usize;
 
@@ -43,24 +51,33 @@ impl Memory {
         match value {
             // 16-bit write
             Integer16(_) | Integer8Pair(_, _) => {
+                // make sure this won't write outside of memory
+                if addr == 0xFFFF {
+                    return Err(String::from("Attempt to write outside of memory"));
+                }
+
+                // convert value to u16
                 let value = u16::from(value);
 
                 // write little-endian 16-bit integer to memory
                 let lower = (value & 0xFF) as u8;
                 let higher = (value >> 8) as u8;
 
-                self.data[addr % 0x10000] = lower;
-                self.data[(addr + 1) % 0x10000] = higher;
+                self.data[addr] = lower;
+                self.data[addr + 1] = higher;
             }
 
             // 8-bit write
             Integer8(_) => {
-                let value = u8::try_from(value).unwrap();
+                // convert value to u8
+                let value = u8::try_from(value)?;
 
                 // write single 8-bit integer to memory
-                self.data[addr % 0x10000] = value;
+                self.data[addr] = value;
             }
         }
+
+        Ok(())
     }
 }
 
@@ -89,7 +106,7 @@ mod tests {
         for i in 0..0x10000 {
             assert_eq!(
                 written_values[i],
-                memory.read(RegisterValue::from(i as u16), false)
+                memory.read(RegisterValue::from(i as u16), false).unwrap()
             );
         }
     }
@@ -100,24 +117,53 @@ mod tests {
 
         // write 0xABCD to address 0x1000, ensure that it will be read back correctly
         // both when reading a single 16-bit value and two 8-bit values
-        memory.write(
-            RegisterValue::from(0x1000u16),
-            RegisterValue::from(0xABCDu16),
-        );
+        memory
+            .write(
+                RegisterValue::from(0x1000u16),
+                RegisterValue::from(0xABCDu16),
+            )
+            .unwrap();
 
         assert_eq!(
-            memory.read(RegisterValue::from(0x1000u16), true),
+            memory.read(RegisterValue::from(0x1000u16), true).unwrap(),
             RegisterValue::from(0xABCDu16)
         );
 
         assert_eq!(
-            memory.read(RegisterValue::from(0x1000u16), false),
+            memory.read(RegisterValue::from(0x1000u16), false).unwrap(),
             RegisterValue::from(0xCDu8)
         );
 
         assert_eq!(
-            memory.read(RegisterValue::from(0x1001u16), false),
+            memory.read(RegisterValue::from(0x1001u16), false).unwrap(),
             RegisterValue::from(0xABu8)
         );
+    }
+
+    #[test]
+    #[should_panic]
+    fn memory_attempt_write_outside_bounds() {
+        let mut memory = Memory::new();
+
+        // try to write a 16-bit value to address 0xFFFF, which would write
+        // outside of memory and should return an Err, which upon .unwrap()
+        // will panic
+        memory
+            .write(
+                RegisterValue::from(0xFFFFu16),
+                RegisterValue::from(0xABCDu16),
+            )
+            .unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn memory_attempt_read_outside_bounds() {
+        let memory = Memory::new();
+
+        // try to read a 16-bit value from address 0xFFFF, which would read
+        // outside of memory and should return an Err, which upon .unwrap()
+        // will panic
+        memory.read(RegisterValue::from(0xFFFFu16), true).unwrap();
     }
 }
