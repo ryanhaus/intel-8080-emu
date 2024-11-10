ab1a9fc23329f95660775a7bc31a5a94c662c409
Ryan Hausmann <ryanhausmann3@gmail.com>
2024-11-10 11:55:47 -0500

    Change formatting of main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 mod cpu;
-use cpu::*;
 use cpu::instruction::*;
 use cpu::registers::*;
+use cpu::*;
 
 fn main() {
     let mut cpu = Cpu::new();
 
     for i in 0..=255 {
         let opcode = i as u8;
-        let decoded =
-            Instruction::decode(RegisterValue::from(opcode));
+        let decoded = Instruction::decode(RegisterValue::from(opcode));
 
         match decoded {
             Ok(instr) => {
