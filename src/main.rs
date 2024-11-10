14b6494f23ce2c2f9f18075ebce9cced21f86c4c
Ryan Hausmann <ryanhausmann3@gmail.com>
2024-11-10 11:46:07 -0500

    Add code that decodes every possible 8-bit integer for verification purposes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 mod cpu;
 use cpu::*;
+use cpu::instruction::*;
+use cpu::registers::*;
 
 fn main() {
     let mut cpu = Cpu::new();
+
+    for i in 0..=255 {
+        let opcode = i as u8;
+        let decoded =
+            Instruction::decode(RegisterValue::from(opcode));
+
+        match decoded {
+            Ok(instr) => {
+                println!("{i:08b} ({i:02X}): {instr:?}");
+            }
+
+            Err(message) => {
+                println!("{i:08b} ({i:02X}): ERROR {message}");
+            }
+        }
+    }
 }
