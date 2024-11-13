1cc7ae94c4baf78adc9ba9d5883e0b5d69eae772
Ryan Hausmann <ryanhausmann3@gmail.com>
2024-11-13 13:04:04 -0500

    Include an 8080 test program; add support for the CP/M 0x0005 BIOS subroutine

--- src/main.rs
+++ src/main.rs
@@ -17,30 +17,46 @@ fn port_handler(port: RegisterValue, value: RegisterValue) {
 
 fn main() {
     let mut cpu = Cpu::new();
+    
+    // CP/M 'bios' containing just the subroutine for 0x0005
+    let cpm_bios_program = vec![
+        // start by jumping to 0x100
+        0xC3, 0x00, 0x01, // JMP 0x0100
+        0x00, // NOP
+        0x00, // NOP
 
-    //let program = include_bytes!("8080tests.bin");
-    //let program = Vec::from(program);
-    let program = vec![
-        0x01, 0x06, 0x00, // LXI BC, 6
-        0xC3, 0x14, 0x00, // JMP loop
-        // data:
-        0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x2C, 0x20, 0x77, 0x6F, 0x72, 0x6C, 0x64, 0x21, 0x0A,
-        // loop:
-        0x0A, // LDAX BC
-        0x03, // INX BC
+        // 0x05 subroutine:
+        //  if C == 9:
+        //      output string starting at (DE) until $ character is found
+        //  if C == 2:
+        //      output single character in E
+        
+        0x79, // MOV A,C
+        0xFE, 0x09, // CPI 9
+        0xC2, 0x17, 0x00, // JNZ not_nine
+        // str_loop:
+        0x1A, // LDAX DE
+        0x13, // INX DE
+        0xFE, 0x24, // CPI '$'
+        0xCA, 0x1F, 0x00, // JZ done
         0xD3, 0x00, // OUT 0
-        0x79, // MOV A, C
-        0xFE, 0x14, // CPI 20
-        0xC2, 0x14, 0x00, // JNZ loop
-        0x76, // HLT
+        0xC3, 0x0B, 0x00, // JMP str_loop
+        // not_nine:
+        0xFE, 0x02, // CPI 2
+        0xC2, 0x1F, 0x00, // JNZ done
+        0x7B, // MOV A,E
+        0xD3, 0x00, // OUT 0
+        // done:
+        0xC9, // RET
     ];
+    let program = include_bytes!("TST8080.COM");
+    let program = Vec::from(program);
 
-    cpu.load_to_memory(program, 0).unwrap();
+    cpu.load_to_memory(cpm_bios_program, 0).unwrap();
+    cpu.load_to_memory(program, 0x100).unwrap();
     cpu.set_port_handler_fn(port_handler);
 
     while cpu.is_running() {
         cpu.execute_next().unwrap();
     }
-
-    // println!("{cpu:#X?}\n");
 }
