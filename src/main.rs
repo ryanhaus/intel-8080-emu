00a4d2c4ebf4035ac8e49e488b8d586333ff85ee
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-04-15 18:52:02 -0400

    Add CLI argument infrastructure & handle debug flag

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,24 @@
-mod cp_m;
+use clap::Parser;
+mod arguments;
 mod cpu;
+mod cp_m;
 mod debug_menu;
 
 use cpu::*;
 use debug_menu::*;
 use std::sync::{Arc, Mutex};
-use std::{env, fs, thread};
+use std::{fs, thread};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = arguments::Args::parse();
 
-    if args.len() != 2 {
-        println!("Usage: {} program", args[0]);
-        return;
-    }
+    let program = fs::read(args.program).unwrap();
 
-    let program_name = &args[1];
-    let program = fs::read(program_name).unwrap();
+    let cpu_output_str = Arc::new(Mutex::new(String::new())); // string containing the output of
+                                                              // the cpu through port 0
+    let cpu_output_str_thr = cpu_output_str.clone(); // clone to be passed to the thread
 
-    let cpu_output_str = Arc::new(Mutex::new(String::new()));
-    let cpu_output_str_thr = cpu_output_str.clone();
-
-    thread::spawn(move || {
+    let sim_handler = move || {
         let mut cpu = Cpu::new();
         cp_m::add_cpm_bdos(&mut cpu);
         cpu.set_pc(0x100).unwrap();
@@ -52,16 +49,22 @@ fn main() {
         }
 
         println!();
-    });
+    };
 
-    init_debug_menu(|ui| {
-        ui.window("Output")
-            .size([300.0, 110.0], imgui::Condition::FirstUseEver)
-            .build(|| {
-                let cpu_output_str = Arc::clone(&cpu_output_str);
-                let out_str = cpu_output_str.lock().unwrap();
+    if args.debug {
+        thread::spawn(sim_handler);
 
-                ui.text_wrapped(out_str.clone());
-            });
-    });
+        init_imgui("Intel 8080 Emulator", |ui| {
+            ui.window("Output")
+                .size([300.0, 110.0], imgui::Condition::FirstUseEver)
+                .build(|| {
+                    let cpu_output_str = Arc::clone(&cpu_output_str);
+                    let out_str = cpu_output_str.lock().unwrap();
+
+                    ui.text_wrapped(out_str.clone());
+                });
+        });
+    } else {
+        sim_handler();
+    }
 }
